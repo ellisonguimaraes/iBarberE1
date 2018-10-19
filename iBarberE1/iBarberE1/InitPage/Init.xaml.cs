50d955d5fa542f367d1ca1435f8797c6b02c6216
Ellison Guimarães <ellison.guimaraes@outlook.com>
2018-10-18 21:36:53 -0300

    Pj1

---
+++ iBarberE1/iBarberE1/InitPage/Init.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using ZXing.Net.Mobile.Forms;
+
+
+namespace iBarberE1.InitPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Init : ContentPage
+	{
+
+        ZXingScannerPage scanPage;
+
+        public Init ()
+		{
+            InitializeComponent();
+
+
+            var tapQrCodeButton = new TapGestureRecognizer();
+            var tapRLButtonOk = new TapGestureRecognizer();
+
+            //Binding events    
+            tapQrCodeButton.Tapped += QrCodeButton_Tapped;
+            tapRLButtonOk.Tapped += RLButtonOk_Tapped;
+
+            //Associating tap events to the image buttons    
+            QrCodeButton.GestureRecognizers.Add(tapQrCodeButton);
+            RLButtonOk.GestureRecognizers.Add(tapRLButtonOk);
+
+        }
+
+
+        private async void QrCodeButton_Tapped(object sender, EventArgs args)
+        {
+            scanPage = new ZXingScannerPage();
+            scanPage.OnScanResult += (result) => {
+                scanPage.IsScanning = false;
+
+                //Do something with result
+                Device.BeginInvokeOnMainThread(() => {
+                    EntryCode.Text = result.Text;
+                    Navigation.PopModalAsync();
+                    DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+
+            
+
+            await Navigation.PushModalAsync(scanPage);
+        }
+
+
+
+        void RLButtonOk_Tapped(object sender, EventArgs  args)
+        {
+            App.Current.MainPage = new MainPage.Main();
+        }
+
+
+
+        private void LoginButton(object sender, EventArgs args)
+        {
+            DisplayAlert("Entrou", "LoginButton", "OK");
+        }
+    }
+}
---
+++ iBarberE1/iBarberE1/MainPage/Main.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace iBarberE1.MainPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Main : ContentPage
+	{
+		public Main ()
+		{
+			InitializeComponent ();
+
+            
+            var tapAgendarButton = new TapGestureRecognizer();
+
+            //Binding events
+            tapAgendarButton.Tapped += Agendar_Tapped;
+
+            //Associating tap events to the image buttons 
+            AgendarButton.GestureRecognizers.Add(tapAgendarButton);
+        }
+
+        private void Agendar_Tapped(object sender, EventArgs args)
+        {
+            DisplayAlert("Agendar", "Agendar_Tapped", "OK");
+        }
+
+
+        private void LbLerMais(object sender, EventArgs args)
+        {
+            if (!End2.IsVisible)
+            {
+                End2.IsVisible = true;
+                End3.IsVisible = true;
+
+                MoreView.Text = "Ocultar";
+            }
+            else
+            {
+                End2.IsVisible = false;
+                End3.IsVisible = false;
+
+                MoreView.Text = "Ver mais";
+            }
+            
+
+            //LblText.Text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
+        }
+
+    }
+}
